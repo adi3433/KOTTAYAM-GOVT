72b1061f273f1361bc1e01d81f421135e98cfb4e
adi3433 <96505738+adi3433@users.noreply.github.com>
2026-04-08 08:45:31 +0530

    fix: resolve ThemeProvider type error causing Vercel build failure

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,16 +3,9 @@
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
   useTheme as useNextTheme,
 } from 'next-themes'
-
-type ThemeProviderProps = {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
-}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
